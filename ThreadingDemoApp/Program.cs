80b86c246aba39e8cc8b07bd0f601fc622839379
Tobi Ulm <tu@tobiasulm.net>
2025-05-23 15:07:33 +0200

    Thrading Synchronization

--- ThreadingDemoApp/Program.cs
+++ ThreadingDemoApp/Program.cs
@@ -6,12 +6,13 @@ namespace ThreadingDemoApp
     internal class Program
     {
         internal static int I = 0;
+        public static object LOCKVAR = "";
 
 
         static void Main(string[] args)
         {
             Thread[] threads = new Thread[100];
-            for (int j = 0;  j < threads.GetUpperBound(0);  j++)
+            for (int j = 0; j < threads.GetUpperBound(0); j++)
             {
                 threads[j] = new Thread(new ThreadStart(ThreadMethod));
                 threads[j].Name = $"Thread {j}";
@@ -23,12 +24,15 @@ namespace ThreadingDemoApp
 
         internal static void ThreadMethod()
         {
-            string threadName = Thread.CurrentThread.Name ?? "Unknown";
-            string threadId = Thread.CurrentThread.ManagedThreadId.ToString();
-            string message = $"Thread gestartet:\t{threadName}\t{threadId}\t";
-            I++;
-            message += $"I = {I}";
-            Console.WriteLine(message);
+            lock (LOCKVAR)
+            {
+                string threadName = Thread.CurrentThread.Name ?? "Unknown";
+                string threadId = Thread.CurrentThread.ManagedThreadId.ToString();
+                string message = $"Thread gestartet:\t{threadName}\t{threadId}\t";
+                I++;
+                message += $"I = {I}";
+                Console.WriteLine(message);
+            }
         }
     }
 }
