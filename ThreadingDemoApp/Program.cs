ad1bc1126ac9324a8118a0f0aed2d5cfafff27ce
Tobi Ulm <tu@tobiasulm.net>
2025-05-23 14:57:59 +0200

    Threading Project erstellt und erstes Threading Sample

---
+++ ThreadingDemoApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingDemoApp
+{
+    internal class Program
+    {
+        internal static int I = 0;
+
+
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[100];
+            for (int j = 0;  j < threads.GetUpperBound(0);  j++)
+            {
+                threads[j] = new Thread(new ThreadStart(ThreadMethod));
+                threads[j].Name = $"Thread {j}";
+                threads[j].Start();
+            }
+            Console.ReadLine();
+        }
+
+
+        internal static void ThreadMethod()
+        {
+            string threadName = Thread.CurrentThread.Name ?? "Unknown";
+            string threadId = Thread.CurrentThread.ManagedThreadId.ToString();
+            string message = $"Thread gestartet:\t{threadName}\t{threadId}\t";
+            I++;
+            message += $"I = {I}";
+            Console.WriteLine(message);
+        }
+    }
+}
